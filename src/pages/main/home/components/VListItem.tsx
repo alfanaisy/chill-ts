0e1c0e5c4cce74f4903f8449ea6caa058341e8bf
alfanaisy <alfanaisy7@gmail.com>
2024-06-16 21:54:59 +0700

    Fix: My List Grid

--- src/pages/main/home/components/VListItem.tsx
+++ src/pages/main/home/components/VListItem.tsx
@@ -29,13 +29,13 @@ const VListItem = ({ image, isTop = false, labels, title, width }: Props) => {
         </div>
       )}
 
-      <div className="absolute bottom-0 left-0 w-full h-3/4 md:h-1/2 p-4 bg-black bg-opacity-80 text-white rounded-md opacity-0 transform translate-y-4 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0">
-        <h3 className="text-sm md:text-base font-bold">{title}</h3>
+      <div className="absolute bottom-0 left-0 w-full h-full sm:h-3/4 md:h-1/2 p-4 bg-black bg-opacity-80 text-white rounded-md opacity-0 transform translate-y-4 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0">
+        <h3 className="text-xs sm:text-sm md:text-base font-bold">{title}</h3>
         <div className="mt-2 flex space-x-2 items-center justify-around">
-          <button className="h-5 w-5 md:h-8 md:w-8 bg-white rounded-full flex-none flex items-center justify-center text-2xl text-black">
+          <button className="sm:h-5 sm:w-5 md:h-8 md:w-8 bg-white rounded-full flex-none flex items-center justify-center text-2xl text-black">
             <BiPlay />
           </button>
-          <button className="px-2 py-1 md:px-3 md:py-1.5 text-sm md:text-base bg-gray-700 text-white rounded">
+          <button className="px-2 py-1 md:px-3 md:py-1.5 text-xs sm:text-sm md:text-base bg-gray-700 text-white rounded">
             Details
           </button>
         </div>
--- src/pages/main/my-list/components/MyList.tsx
+++ src/pages/main/my-list/components/MyList.tsx
@@ -5,7 +5,7 @@ const MyList = ({ items }: { items: ItemListType[] }) => {
   return (
     <div className="text-white">
       <h2 className="text-xl md:text-2xl font-bold mb-4">Daftar Saya</h2>
-      <div className="grid grid-cols-3 md:grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-4">
+      <div className="grid grid-cols-3 md:grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-x-4 gap-y-6">
         {items.map((item) => (
           <VListItem
             width="min-w-20 md:min-w-40 xl:min-w-48"
